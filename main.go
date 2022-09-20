ba06d77b0c62f93ae00da44b32c715cddabf0b98
Joe Chen <jc@unknwon.io>
2022-09-20 23:26:32 +0800

    Stub implementation of the server

--- main.go
+++ main.go
@@ -4,6 +4,31 @@
 
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/flamego/flamego"
+	log "unknwon.dev/clog/v2"
+
+	"github.com/codenotify/codenotify.run/internal/conf"
+)
+
+func main() {
+	if err := log.NewConsole(); err != nil {
+		panic(err)
+	}
+
+	log.Info("Codenotify as a Service!")
+	if conf.BuildTime != "" {
+		log.Info("Build time: %s", conf.BuildTime)
+		log.Info("Build commit: %s", conf.BuildCommit)
+	}
+
+	f := flamego.Classic()
+	f.Post("/-/webhook", func(r *http.Request) string {
+		event := r.Header.Get("X-GitHub-Event")
+		log.Trace("Received event: %s", event)
+		return event
+	})
+	f.Run()
 }
