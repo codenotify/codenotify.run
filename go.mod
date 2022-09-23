8a234fa92753b9a4a02e8abd066ebab8a99a44aa
Joe Chen <jc@unknwon.io>
2022-09-23 23:31:56 +0800

    Support post new comment on PR creation

--- go.mod
+++ go.mod
@@ -3,15 +3,26 @@ module github.com/codenotify/codenotify.run
 go 1.19
 
 require (
+	github.com/bradleyfalzon/ghinstallation/v2 v2.1.0
 	github.com/flamego/flamego v1.5.0
+	github.com/go-ini/ini v1.67.0
+	github.com/google/go-github/v45 v45.2.0
+	github.com/pkg/errors v0.9.1
+	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1
 	unknwon.dev/clog/v2 v2.2.0
 )
 
 require (
 	github.com/alecthomas/participle/v2 v2.0.0-alpha9 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,9 +5,16 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/flamego/flamego"
+	"github.com/google/go-github/v45/github"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
 	log "unknwon.dev/clog/v2"
 
 	"github.com/codenotify/codenotify.run/internal/conf"
@@ -24,11 +31,112 @@ func main() {
 		log.Info("Build commit: %s", conf.BuildCommit)
 	}
 
+	config, err := conf.Load()
+	if err != nil {
+		log.Fatal("Failed to load configuration: %v", err)
+	}
+
 	f := flamego.Classic()
-	f.Post("/-/webhook", func(r *http.Request) string {
+	f.Post("/-/webhook", func(r *http.Request) (int, string) {
 		event := r.Header.Get("X-GitHub-Event")
 		log.Trace("Received event: %s", event)
-		return event
+
+		switch event {
+		case "pull_request":
+			var payload struct {
+				Action      string `json:"action"`
+				PullRequest struct {
+					URL    string `json:"url"`
+					Number int    `json:"number"`
+				} `json:"pull_request"`
+				Repository struct {
+					Name  string `json:"name"`
+					Owner struct {
+						Login string `json:"login"`
+					} `json:"owner"`
+				} `json:"repository"`
+				Installation struct {
+					ID int64 `json:"id"`
+				} `json:"installation"`
+			}
+			err = json.NewDecoder(r.Body).Decode(&payload)
+			if err != nil {
+				return http.StatusBadRequest, fmt.Sprintf("Failed to decode payload: %v", err)
+			}
+			if payload.Action != "opened" {
+				return http.StatusOK, fmt.Sprintf("Event %q with action %q has been received but nothing to do", event, payload.Action)
+			}
+
+			if payload.Installation.ID <= 0 {
+				return http.StatusBadRequest, "No installation ID"
+			}
+
+			go func() {
+				err = createPullRequestComment(
+					config.GitHubApp.AppID,
+					payload.Installation.ID,
+					config.GitHubApp.PrivateKey,
+					payload.Repository.Owner.Login,
+					payload.Repository.Name,
+					payload.PullRequest.Number,
+				)
+				if err != nil {
+					log.Error("Failed to create comment on pull request %s: %v", payload.PullRequest.URL, err)
+					return
+				}
+
+				log.Info("Created comment on pull request %s", payload.PullRequest.URL)
+			}()
+			return http.StatusAccepted, http.StatusText(http.StatusAccepted)
+		}
+		return http.StatusOK, fmt.Sprintf("Event %q has been received but nothing to do", event)
 	})
 	f.Run()
+}
+
+func createPullRequestComment(appID, installationID int64, privateKey, owner, repo string, number int) error {
+	tr, err := ghinstallation.NewAppsTransport(http.DefaultTransport, appID, []byte(privateKey))
+	if err != nil {
+		return errors.Wrap(err, "new transport")
+	}
+
+	client := github.NewClient(
+		&http.Client{
+			Transport: tr,
+		},
+	)
+
+	ctx := context.Background()
+	token, _, err := client.Apps.CreateInstallationToken(ctx, installationID, nil)
+	if err != nil {
+		return errors.Wrap(err, "create installation access token")
+	}
+	if token.Token == nil || *token.Token == "" {
+		return errors.New("empty token returned")
+	}
+
+	client = github.NewClient(
+		oauth2.NewClient(
+			ctx,
+			oauth2.StaticTokenSource(
+				&oauth2.Token{
+					AccessToken: *token.Token,
+				},
+			),
+		),
+	)
+
+	_, _, err = client.Issues.CreateComment(
+		ctx,
+		owner,
+		repo,
+		number,
+		&github.IssueComment{
+			Body: github.String("Hello world!"),
+		},
+	)
+	if err != nil {
+		return errors.Wrap(err, "create comment")
+	}
+	return nil
 }
